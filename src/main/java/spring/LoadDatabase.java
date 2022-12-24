721de2b3a9ae540320a1e983d8f1a044a6c87d9f
DonToli <strt87@yandex.ru>
2022-12-24 12:15:44 +0300

    Переделано под тест

--- src/main/java/spring/LoadDatabase.java
+++ src/main/java/spring/LoadDatabase.java
@@ -8,18 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import repository.EmployeeRepository;
 
+
 @Configuration
 class LoadDatabase {
-
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
-    @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
-
-        return args -> {
-            log.info("Preloading " + repository.save(new Employee(5)));
-            log.info("Preloading " + repository.save(new Employee(7)));
-        };
-
-    }
+//
+//    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+//
+//    @Bean
+//    CommandLineRunner initDatabase(EmployeeRepository repository) {
+//
+//        return args -> {
+//            log.info("Preloading " + repository.save(new Employee(5)));
+//            log.info("Preloading " + repository.save(new Employee(7)));
+//        };
+//
+//    }
 }
